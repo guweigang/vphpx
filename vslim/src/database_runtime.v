e3da9c0c1bec4bbd2958a225b8650f6b691c410d
weigang <guweigang@bullsoft.org>
2026-04-26 07:25:27 +0800

    vslim: fix linux build for mysql plugin helper

--- vslim/src/database_runtime.v
+++ vslim/src/database_runtime.v
@@ -7,36 +7,7 @@ import vphp
 
 $if linux {
 	#flag -ldl
-	#include <dlfcn.h>
-	#include <limits.h>
-	#include <stdlib.h>
-	#include <string.h>
-
-	char* vslim_mysql_client_plugin_dir(void) {
-		Dl_info info;
-		if (dladdr((void*) mysql_init, &info) == 0 || info.dli_fname == NULL) {
-			return NULL;
-		}
-		size_t len = strlen(info.dli_fname);
-		char* path = (char*) malloc(PATH_MAX);
-		if (path == NULL || len + 1 >= PATH_MAX) {
-			free(path);
-			return NULL;
-		}
-		memcpy(path, info.dli_fname, len + 1);
-		char* slash = strrchr(path, '/');
-		if (slash == NULL) {
-			free(path);
-			return NULL;
-		}
-		*slash = '\0';
-		if (strlen(path) + strlen("/plugin") + 1 >= PATH_MAX) {
-			free(path);
-			return NULL;
-		}
-		strcat(path, "/plugin");
-		return path;
-	}
+	#insert "@VMODROOT/database_runtime_linux_helper.h"
 	fn C.vslim_mysql_client_plugin_dir() &char
 	fn C.free(voidptr)
 }
