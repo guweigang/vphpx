524cfbaee207c27f1e67129357da2c4a1897f863
weigang <guweigang@bullsoft.org>
2026-05-13 14:25:40 +0800

    Route closure creation through Zend wrapper

--- vphp/php_closure_invoke.v
+++ vphp/php_closure_invoke.v
@@ -12,12 +12,12 @@ fn save_closure_value[T](v_cb T) voidptr {
 }
 
 pub fn (ctx Context) create_saved_closure[T](v_cb T, bridge voidptr, arity int) {
-	C.vphp_create_closure_with_arity(ctx.ret.raw_zval(), save_closure_value[T](v_cb), bridge,
-		arity, arity)
+	zend_create_closure_with_arity(ctx.ret.raw_zval(), save_closure_value[T](v_cb), bridge, arity,
+		arity)
 }
 
 pub fn (ctx Context) create_saved_variadic_closure[T](v_cb T, bridge voidptr) {
-	C.vphp_create_variadic_closure(ctx.ret.raw_zval(), save_closure_value[T](v_cb), bridge)
+	zend_create_variadic_closure(ctx.ret.raw_zval(), save_closure_value[T](v_cb), bridge)
 }
 
 pub fn (ctx Context) invoke_variadic_closure[T, R](cb T) {
---
+++ vphp/zend_closure.v
@@ -0,0 +1,11 @@
+module vphp
+
+import vphp.zend as _
+
+fn zend_create_closure_with_arity(ret &C.zval, thunk voidptr, bridge voidptr, num_args int, required_args int) {
+	C.vphp_create_closure_with_arity(ret, thunk, bridge, num_args, required_args)
+}
+
+fn zend_create_variadic_closure(ret &C.zval, thunk voidptr, bridge voidptr) {
+	C.vphp_create_variadic_closure(ret, thunk, bridge)
+}
