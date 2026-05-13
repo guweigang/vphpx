ad1c13f01cfa3eb09c8b92fc96e79c24adf191ed
weigang <guweigang@bullsoft.org>
2026-05-13 19:06:35 +0800

    Group object return Zend calls

--- vphp/object_binding.v
+++ vphp/object_binding.v
@@ -12,18 +12,30 @@ pub fn (z ZVal) bind_borrowed_object(handlers voidptr) {
 	z.bind_object(handlers, .borrowed)
 }
 
-fn return_unbound_object_to(ret PhpReturn, v_ptr voidptr, ce voidptr) {
+fn zend_return_unbound_object(ret PhpReturn, v_ptr voidptr, ce voidptr) {
 	C.vphp_return_obj(ret.raw_zval(), v_ptr, ce)
+}
+
+fn zend_return_borrowed_object(ret PhpReturn, v_ptr voidptr, ce voidptr, handlers voidptr) {
+	C.vphp_return_borrowed_object(ret.raw_zval(), v_ptr, ce, handlers)
+}
+
+fn zend_return_owned_object(ret PhpReturn, v_ptr voidptr, ce voidptr, handlers voidptr) {
+	C.vphp_return_owned_object(ret.raw_zval(), v_ptr, ce, handlers)
+}
+
+fn return_unbound_object_to(ret PhpReturn, v_ptr voidptr, ce voidptr) {
+	zend_return_unbound_object(ret, v_ptr, ce)
 }
 
 fn return_bound_object_to(ret PhpReturn, v_ptr voidptr, ce voidptr, handlers voidptr, ownership OwnershipKind) {
 	match ownership {
 		.borrowed {
-			C.vphp_return_borrowed_object(ret.raw_zval(), v_ptr, ce, handlers)
+			zend_return_borrowed_object(ret, v_ptr, ce, handlers)
 		}
 		.owned_request, .owned_persistent {
 			register_vptr_root(v_ptr)
-			C.vphp_return_owned_object(ret.raw_zval(), v_ptr, ce, handlers)
+			zend_return_owned_object(ret, v_ptr, ce, handlers)
 		}
 	}
 }
