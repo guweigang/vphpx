77c60c52cdbe8e79330aaa02aeb602a982b2e79f
weigang <guweigang@bullsoft.org>
2026-05-14 14:03:27 +0800

    Use PhpReturn raw pointer for closure creation

--- vphp/zend/closure.v
+++ vphp/zend/closure.v
@@ -4,6 +4,14 @@ pub fn create_closure_with_arity(ret &C.zval, thunk voidptr, bridge voidptr, num
 	C.vphp_create_closure_with_arity(ret, thunk, bridge, num_args, required_args)
 }
 
+pub fn create_closure_with_arity_ptr(ret voidptr, thunk voidptr, bridge voidptr, num_args int, required_args int) {
+	create_closure_with_arity(unsafe { &C.zval(ret) }, thunk, bridge, num_args, required_args)
+}
+
 pub fn create_variadic_closure(ret &C.zval, thunk voidptr, bridge voidptr) {
 	C.vphp_create_variadic_closure(ret, thunk, bridge)
+}
+
+pub fn create_variadic_closure_ptr(ret voidptr, thunk voidptr, bridge voidptr) {
+	create_variadic_closure(unsafe { &C.zval(ret) }, thunk, bridge)
 }
