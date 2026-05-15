d8ba290f0b0c179b4b37634e4bd14d9b11660f04
weigang <guweigang@bullsoft.org>
2026-05-15 11:07:07 +0800

    Route PhpReturn object binding directly

--- vphp/object_binding.v
+++ vphp/object_binding.v
@@ -1,7 +1,5 @@
 module vphp
 
-import vphp.object
-
 pub fn (z ZVal) bind_object(handlers voidptr, ownership OwnershipKind) {
 	ZendObject.from_zval(z).bind_handlers(handlers, ownership)
 }
@@ -12,31 +10,4 @@ pub fn (z ZVal) bind_owned_object(handlers voidptr) {
 
 pub fn (z ZVal) bind_borrowed_object(handlers voidptr) {
 	z.bind_object(handlers, .borrowed)
-}
-
-fn zend_return_unbound_object(ret PhpReturn, v_ptr voidptr, ce ZendClassEntry) {
-	object.return_unbound(ret.raw_ptr(), v_ptr, ce.raw_ptr())
-}
-
-fn zend_return_borrowed_object(ret PhpReturn, v_ptr voidptr, ce ZendClassEntry, handlers voidptr) {
-	object.return_bound(ret.raw_ptr(), v_ptr, ce.raw_ptr(), handlers, .borrowed)
-}
-
-fn zend_return_owned_object(ret PhpReturn, v_ptr voidptr, ce ZendClassEntry, handlers voidptr) {
-	object.return_bound(ret.raw_ptr(), v_ptr, ce.raw_ptr(), handlers, .owned)
-}
-
-fn return_unbound_object_to(ret PhpReturn, v_ptr voidptr, ce ZendClassEntry) {
-	zend_return_unbound_object(ret, v_ptr, ce)
-}
-
-fn return_bound_object_to(ret PhpReturn, v_ptr voidptr, ce ZendClassEntry, handlers voidptr, ownership OwnershipKind) {
-	match ownership {
-		.borrowed {
-			zend_return_borrowed_object(ret, v_ptr, ce, handlers)
-		}
-		.owned_request, .owned_persistent {
-			zend_return_owned_object(ret, v_ptr, ce, handlers)
-		}
-	}
 }
