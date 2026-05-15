67d17c9217cc836b7de43b5d946600d812e2430f
weigang <guweigang@bullsoft.org>
2026-05-15 10:51:03 +0800

    Trim reference and resource zval forwarding helpers

--- vphp/zval_reference.v
+++ vphp/zval_reference.v
@@ -2,19 +2,11 @@ module vphp
 
 import vphp.zval
 
-fn zend_reference_value(v ZVal) zval.Handle {
-	return zval.reference_value(v.handle())
-}
-
-fn zend_reference_set_zval(v ZVal, value ZVal) {
-	zval.set_reference_value(v.handle(), value.handle())
-}
-
 pub fn (v ZVal) reference_value() ZVal {
 	if !v.is_valid() {
 		return invalid_zval()
 	}
-	handle := zend_reference_value(v)
+	handle := zval.reference_value(v.handle())
 	if !handle.is_valid() {
 		return invalid_zval()
 	}
@@ -25,5 +17,5 @@ pub fn (v ZVal) set_reference_value(value ZVal) {
 	if !v.is_valid() || !value.is_valid() {
 		return
 	}
-	zend_reference_set_zval(v, value)
+	zval.set_reference_value(v.handle(), value.handle())
 }
--- vphp/zval_resource.v
+++ vphp/zval_resource.v
@@ -2,24 +2,16 @@ module vphp
 
 import vphp.zval
 
-fn zend_make_resource(v ZVal, ptr voidptr, label string) {
-	zval.make_resource(v.handle(), ptr, label)
-}
-
-fn zend_fetch_resource(v ZVal) voidptr {
-	return zval.resource_ptr(v.handle())
-}
-
 pub fn (v ZVal) make_resource(ptr voidptr, label string) {
 	if !v.is_valid() {
 		return
 	}
-	zend_make_resource(v, ptr, label)
+	zval.make_resource(v.handle(), ptr, label)
 }
 
 pub fn (v ZVal) resource_ptr() voidptr {
 	if !v.is_valid() || !v.is_resource() {
 		return unsafe { nil }
 	}
-	return zend_fetch_resource(v)
+	return zval.resource_ptr(v.handle())
 }
