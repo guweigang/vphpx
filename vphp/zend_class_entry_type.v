fe60cd95c48dbbe1135500738d7a2c2e1ecd8935
weigang <guweigang@bullsoft.org>
2026-05-15 11:09:12 +0800

    Trim class entry forwarding helpers

--- vphp/zend_class_entry_type.v
+++ vphp/zend_class_entry_type.v
@@ -20,40 +20,16 @@ pub fn (ce ZendClassEntry) raw_ptr() voidptr {
 	return ce.raw
 }
 
-fn zend_class_set_static_long(ce ZendClassEntry, name string, val i64) {
-	zend.set_static_long(ce.raw_ptr(), name, val)
-}
-
-fn zend_class_set_static_string(ce ZendClassEntry, name string, val string) {
-	zend.set_static_string(ce.raw_ptr(), name, val)
-}
-
-fn zend_class_set_static_bool(ce ZendClassEntry, name string, val bool) {
-	zend.set_static_bool(ce.raw_ptr(), name, val)
-}
-
-fn zend_class_static_long(ce ZendClassEntry, name string) i64 {
-	return zend.static_long(ce.raw_ptr(), name)
-}
-
-fn zend_class_static_string(ce ZendClassEntry, name string) string {
-	return zend.static_string(ce.raw_ptr(), name)
-}
-
-fn zend_class_static_bool(ce ZendClassEntry, name string) bool {
-	return zend.static_bool(ce.raw_ptr(), name)
-}
-
 pub fn (ce ZendClassEntry) set_static_prop[T](name string, val T) {
 	if !ce.is_valid() {
 		return
 	}
 	$if T is int {
-		zend_class_set_static_long(ce, name, i64(val))
+		zend.set_static_long(ce.raw_ptr(), name, i64(val))
 	} $else $if T is string {
-		zend_class_set_static_string(ce, name, val)
+		zend.set_static_string(ce.raw_ptr(), name, val)
 	} $else $if T is bool {
-		zend_class_set_static_bool(ce, name, val)
+		zend.set_static_bool(ce.raw_ptr(), name, val)
 	}
 }
 
@@ -62,11 +38,11 @@ pub fn (ce ZendClassEntry) static_prop[T](name string) T {
 		return T{}
 	}
 	$if T is int {
-		return int(zend_class_static_long(ce, name))
+		return int(zend.static_long(ce.raw_ptr(), name))
 	} $else $if T is string {
-		return zend_class_static_string(ce, name)
+		return zend.static_string(ce.raw_ptr(), name)
 	} $else $if T is bool {
-		return zend_class_static_bool(ce, name)
+		return zend.static_bool(ce.raw_ptr(), name)
 	}
 	return T{}
 }
