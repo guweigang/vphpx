8a65e03015fb0f67e0434a37e3d9953d4a3ef200
weigang <guweigang@bullsoft.org>
2026-07-19 18:56:05 +0800

    chore: 将 plugin.php 和 class-wp-hook.php 移出 allowed_includes 以让其完全在 Zend 侧运行，解决 apply_filters 等核心函数与 WP_Hook 类未在 Zend 注册的未定义错误

--- php2v/src/emitter/transpiler.v
+++ php2v/src/emitter/transpiler.v
@@ -185,8 +185,6 @@ pub fn Transpiler.new() Transpiler {
 		has_dynamic_method_call: false
 		has_dynamic_func_call:   false
 		allowed_includes: [
-			'wp-includes/plugin.php',
-			'wp-includes/class-wp-hook.php',
 			'wp-includes/default-filters.php',
 			'wp-includes/version.php',
 			'wp-includes/compat.php',
