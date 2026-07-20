9abd42cccfe9e34bfa2fa97e313aa57cab278d8a
weigang <guweigang@bullsoft.org>
2026-07-20 11:27:27 +0800

    feat: 在 C 侧动态解释器中补全数据库配置持久常量，支持 wp-content/db.php 动态插件无缝运行

--- php2v/src/rt/rt_helper.h
+++ php2v/src/rt/rt_helper.h
@@ -860,6 +860,12 @@ static inline int php2v_execute_file(const char* filepath) {
     php2v_register_persistent_constant("WPINC", "wp-includes");
     php2v_register_persistent_constant("WP_CONTENT_DIR", "/Users/guweigang/wwwroot/wordpress/wp-content");
     php2v_register_persistent_constant("WP_USE_THEMES", "1");
+    php2v_register_persistent_constant("DB_NAME", "wordpress");
+    php2v_register_persistent_constant("DB_USER", "root");
+    php2v_register_persistent_constant("DB_PASSWORD", "Abcd.1234");
+    php2v_register_persistent_constant("DB_HOST", "localhost");
+    php2v_register_persistent_constant("DB_CHARSET", "utf8mb4");
+    php2v_register_persistent_constant("DB_COLLATE", "");
     php2v_register_persistent_constant("MYSQLI_REPORT_OFF", "0");
     php2v_register_persistent_constant("MYSQLI_REPORT_ERROR", "1");
     php2v_register_persistent_constant("MYSQLI_REPORT_STRICT", "2");
