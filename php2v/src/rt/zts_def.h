fa547ded95fbe029e148b6efac4083c8db0e2029
weigang <guweigang@bullsoft.org>
2026-07-21 08:48:44 +0800

    fix: 移除 USE_ZEND_ALLOC=0 恢复 Zend 专属内存管理器，防止多线程底层 malloc 堆崩溃

--- php2v/src/rt/zts_def.h
+++ php2v/src/rt/zts_def.h
@@ -184,7 +184,6 @@ static void php2v_register_server_variables(zval *track_vars_array) {
 }
 
 __attribute__((constructor)) static void php2v_auto_embed_init() {
-	setenv("USE_ZEND_ALLOC", "0", 1);
 	setenv("PHPRC", "/nonexistent", 1);
 	setenv("PHP_INI_SCAN_DIR", "", 1);
 	php_embed_module.php_ini_ignore = 1;
